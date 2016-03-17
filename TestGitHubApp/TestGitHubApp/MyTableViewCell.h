c6a46e2d2169ad37afe33e3635f6b5832cd9a6ba
付莉 <niuxiaoniu0527@sina.com>
2016-03-17 10:13:23 +0800

    付莉推送 创建cell

---
+++ TestGitHubApp/TestGitHubApp/MyTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MyTableViewCell.h
+//  TestGitHubApp
+//
+//  Created by 付莉 on 16/3/17.
+//  Copyright © 2016年 DYK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyTableViewCell : UITableViewCell
+
+@end
---
+++ TestGitHubApp/TestGitHubApp/MyTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MyTableViewCell.m
+//  TestGitHubApp
+//
+//  Created by 付莉 on 16/3/17.
+//  Copyright © 2016年 DYK. All rights reserved.
+//
+
+#import "MyTableViewCell.h"
+
+@implementation MyTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
